7b7a25ad6ceef0f3379bda80d65c4b3613931c41
JesusCH92 <jcocaharo@hotmail.com>
2024-04-13 15:27:06 +0200

    modelamos

--- src/Driver/Domain/Entity/Driver.php
+++ src/Driver/Domain/Entity/Driver.php
@@ -5,6 +5,9 @@ namespace App\Driver\Domain\Entity;
 use App\Driver\Domain\ValueObject\License;
 use App\Driver\Domain\ValueObject\Name;
 use App\Driver\Domain\ValueObject\Surname;
+use App\Trip\Domain\Entity\Trip;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
@@ -25,11 +28,15 @@ class Driver
     #[ORM\Embedded(class: License::class, columnPrefix: false)]
     private License $license;
 
+    #[ORM\OneToMany(targetEntity: Trip::class, mappedBy: 'driver')]
+    private Collection $trips;
+
     public function __construct(?string $name, ?string $surname, ?string $license)
     {
         $this->name    = new Name($name);
         $this->surname = new Surname($surname);
         $this->license = new License($license);
+        $this->trips = new ArrayCollection();
     }
 
     public function id(): ?int
@@ -55,5 +62,26 @@ class Driver
     public function description(): string
     {
         return (string)$this->name() . ' - ' . (string)$this->surname() . ' - ' . $this->license();
+    }
+
+    public function trips(): Collection
+    {
+        return $this->trips;
+    }
+
+    public function tripCollection(): array
+    {
+        $collection = [];
+
+        foreach ($this->trips() as $trip) {
+            $collection[] = $trip;
+        }
+
+        return $collection;
+    }
+
+    public function isDeletable(): bool
+    {
+        return $this->tripCollection() === [];
     }
 }
