ff26ee4af13424ba924a3336eb3781e9e7c20b78
JesusCH92 <jcocaharo@hotmail.com>
2024-04-13 16:15:13 +0200

    modelamos

--- src/Vehicle/Domain/Entity/Vehicle.php
+++ src/Vehicle/Domain/Entity/Vehicle.php
@@ -2,10 +2,13 @@
 
 namespace App\Vehicle\Domain\Entity;
 
+use App\Trip\Domain\Entity\Trip;
 use App\Vehicle\Domain\ValueObject\Brand;
 use App\Vehicle\Domain\ValueObject\LicenseRequired;
 use App\Vehicle\Domain\ValueObject\Model;
 use App\Vehicle\Domain\ValueObject\Plate;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
@@ -29,12 +32,16 @@ class Vehicle
     #[ORM\Embedded(class: LicenseRequired::class, columnPrefix: false)]
     private LicenseRequired $licenseRequired;
 
+    #[ORM\OneToMany(targetEntity: Trip::class, mappedBy: 'vehicle')]
+    private Collection $trips;
+
     public function __construct(?string $brand, ?string $model, ?string $plate, ?string $licenseRequired)
     {
         $this->brand           = new Brand($brand);
         $this->model           = new Model($model);
         $this->plate           = new Plate($plate);
         $this->licenseRequired = new LicenseRequired($licenseRequired);
+        $this->trips           = new ArrayCollection();
     }
 
     public function id(): ?int
@@ -65,5 +72,26 @@ class Vehicle
     public function description(): string
     {
         return (string)$this->brand() . ' - ' . (string)$this->model() . ' - ' . (string)$this->plate();
+    }
+
+    public function trips(): Collection
+    {
+        return $this->trips;
+    }
+
+    public function tripCollection(): array
+    {
+        $collection = [];
+
+        foreach ($this->trips() as $trip) {
+            $collection[] = $trip;
+        }
+
+        return $collection;
+    }
+
+    public function isDeletable(): bool
+    {
+        return $this->tripCollection() === [];
     }
 }
